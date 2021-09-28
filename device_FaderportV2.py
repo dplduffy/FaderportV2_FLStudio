d61bdbf8cb3aaa43be9722d606cf94314feda824
Dan Duffy <dplduffy@gmail.com>
2021-09-27 21:58:00 -0400

    encoder modes, high res fader, pan controls

--- device_FaderportV2.py
+++ device_FaderportV2.py
@@ -3,6 +3,7 @@
 
 import patterns
 import mixer
+import channels
 import device
 import transport
 import arrangement
@@ -14,33 +15,27 @@ import math
 import midi
 import utils
 
+ENC_MODE = 0
+PREV_NEXT_MODE = 0
+IDLE_COUNT = 0
+PULSE_VAL = 0
+
 class TFaderportV2:
 
 	def OnInit(self):
 		print("On Init")
 		self.BTN = self.BTN()
 		self.STATUS = self.STATUS()
+		self.ENC = self.ENC()
+		self.PN = self.PN()
+		self.UpdateEncMode(2)
+		global PREV_NEXT_MODE
+		PREV_NEXT_MODE = 0
 		self.OnRefresh(0)
 
 	def OnDeInit(self):
 		print("On De Init")
-
-	# def UpdateKnobs(self, Index, Value):
-
-	# 	#print("Update KNOB")
-	# 	if (self.KNOB.PAN == Index):
-	# 		if (64 < Value < 65): Value = 65
-	# 		if (64 > Value > 63): Value = 63
-	# 	Value = int(Value)
-	# 	#print("Knob Output: i=", Index, ", val=", Value)
-	# 	device.midiOutMsg(0 + midi.MIDI_CONTROLCHANGE + (Index << 8) + (Value << 16))
-
-	def UpdateFader(self, Value1, Value2):
-		device.midiOutMsg(self.STATUS.FADER + (Value1 << 8) + (Value2 << 16))
-
-	def UpdateLEDs(self, Index, Value):
-		device.midiOutMsg(self.STATUS.BTN + (Index << 8) + (Value << 16))
-
+		
 	def OnMidiIn(self, event):
 
 		print("On Midi In")
@@ -50,26 +45,39 @@ class TFaderportV2:
 		i = mixer.trackNumber()
 
 		if event.status == self.STATUS.FADER:
-			#TODO involve data 1 and make high res fader
-			sVol = self.scaleValue(event.data2, 127, 1)
-			mixer.setTrackVolume(mixer.trackNumber(), sVol)
+			intVol = self.scaleValue(event.data2, 127, 1)
+			fracVol = self.scaleValue(event.data1, 127, 1)/100
+			sVol = intVol + fracVol
+			mixer.setTrackVolume(i, sVol)
 			event.handled = True
 
 		elif event.status == self.STATUS.KNOB:
-			sPan = (self.scaleValue(event.data1, 127, 2) - 1)
-			if (abs(sPan) < 0.008):
-				sPan = 0
-			mixer.setTrackPan(mixer.trackNumber(), sPan)
-			event.handled = True
+			if ENC_MODE == self.ENC.LINK:
+				event.handled = True
+			elif ENC_MODE == self.ENC.PAN:
+				if event.data2 > 64: 
+					sPan = mixer.getTrackPan(i) + ((64 - event.data2)/100)
+				else:
+					sPan = mixer.getTrackPan(i) + (event.data2/100)
+				mixer.setTrackPan(i, sPan)
+				event.handled = True
+			elif ENC_MODE == self.ENC.CHANNEL:
+				if event.data2 > 64:
+					self.Previous()
+				else:
+					self.Next()
+				event.handled = True
+			elif ENC_MODE == self.ENC.SCROLL:
+				event.handled = True
 
 		elif (event.status == self.STATUS.BTN) & (event.controlVal == 127):
 			
 			if (event.controlNum == self.BTN.PREV_UNDO):
-				transport.globalTransport(midi.FPT_Previous,1)
+				self.Previous()
 				event.handled = True
 
 			if (event.controlNum == self.BTN.NEXT_REDO):
-				transport.globalTransport(midi.FPT_Next,1)
+				self.Next()
 				event.handled = True
 
 			if (event.controlNum == self.BTN.CLICK_F2):
@@ -103,25 +111,29 @@ class TFaderportV2:
 			if (event.controlNum == self.BTN.ARM_ALL):
 				mixer.armTrack(i)
 				event.handled = True
+			
+			if (event.controlNum == self.BTN.LINK_LOCK):
+				self.UpdateEncMode(1)
+				event.handled = True
+
+			if (event.controlNum == self.BTN.PAN_FLIP):
+				self.UpdateEncMode(2)
+				event.handled = True
+
+			if (event.controlNum == self.BTN.CHANNEL_LOCK):
+				self.UpdateEncMode(3)
+				event.handled = True
+
+			if (event.controlNum == self.BTN.SCROLL_ZOOM):
+				self.UpdateEncMode(4)
+				event.handled = True
+
+			if (event.controlNum == self.BTN.KNOB):
+				mixer.setTrackPan(i, 0)
+				event.handled = True
 
 		elif (event.status == self.STATUS.BTN) & (event.controlVal == 0):
 			event.handled = True
-
-	def OnMidiMsg(self, event):
-
-		print("On Midi Msg")
-		event.handled = False
-		
-
-	def OnControlChange(self, event):
-
-		print("On Control Change")
-		event.handled = False
-
-	def OnMidiOutMsg(self, event):
-
-		print("On Midi Out Msg")
-		event.handled = False
 
 	def OnRefresh(self, flags):
 
@@ -131,13 +143,12 @@ class TFaderportV2:
 			i = mixer.trackNumber()
 
 			Volume = mixer.getTrackVolume(i)
-			sVol = round(self.scaleValue(Volume, 1, 127))
-			print(sVol)
-			self.UpdateFader(0, sVol)
+			sVol = math.modf(self.scaleValue(Volume, 1, 127))
+			fracVol = round(self.scaleValue(sVol[0], 1, 127))
+			intVol = round(sVol[1])
+			self.UpdateFader(fracVol, intVol)
 
-			# Pan = 1 + (mixer.getTrackPan(i))
-			# sPan = self.scaleValue(Pan, 2, 127)
-			# self.UpdateKnobs(self.KNOB.PAN, sPan)
+			Pan = (mixer.getTrackPan(i))
 
 			if mixer.isTrackSolo(i):
 				self.UpdateLEDs(self.BTN.SOLO_CLEAR, 127)
@@ -173,31 +184,82 @@ class TFaderportV2:
 				self.UpdateLEDs(self.BTN.LOOPMODE, 0)
 			else:
 				self.UpdateLEDs(self.BTN.LOOPMODE, 127)
-
-	def OnDoFullRefresh(self):
-
-		print("On Do Full Refresh: ")
+			
+			self.UpdateEncMode(ENC_MODE)
+			
+			global PREV_NEXT_MODE
+			if ui.getFocused(0):
+				PREV_NEXT_MODE = 0
+			elif ui.getFocused(1):
+				PREV_NEXT_MODE = 1
 
 	def OnUpdateBeatIndicator(self, value):
 
 		print("On Update Beat Indicator")
 
-	#def OnIdle(self):
+	def OnIdle(self):
 
-		#self.dirtyRefreshMacros()
+		
+		global IDLE_COUNT
+		IDLE_COUNT = IDLE_COUNT + 1
+
+		global PULSE_VAL
+		if IDLE_COUNT == 1:
+			PULSE_VAL = 127
+			self.OnRefresh(0)
+		elif IDLE_COUNT == 13:
+			PULSE_VAL = 0
+			self.OnRefresh(0)
+		elif IDLE_COUNT > 24:
+			IDLE_COUNT = 0
+		
+		print("PULSE : ", PULSE_VAL)
+		
+
 
 	def scaleValue(self, value, scaleIn, scaleOut):
 		return ((value/scaleIn) * scaleOut)
 
-	#def dirtyRefreshMacros(self):
+	def UpdateFader(self, Frac, Int):
+		device.midiOutMsg(self.STATUS.FADER + (Frac << 8) + (Int << 16))
 
-		# print("On Dirty Refresh Macros")
+	def UpdateLEDs(self, Index, Value):
+		device.midiOutMsg(self.STATUS.BTN + (Index << 8) + (Value << 16))
 
-		# for controlId in range(8):
-		# 	eventID = device.findEventID(midi.EncodeRemoteControlID(device.getPortNumber(), 0, 0) + controlId, 1)
-		# 	sVal = self.scaleValue(device.getLinkedValue(eventID), 1, 127)
-		# 	#print("CID: ", controlId, "val: ", sVal)
-		# 	self.UpdateKnobs(controlId, sVal)
+	def UpdateEncMode(self, mode):
+		global ENC_MODE
+		ENC_MODE = mode
+
+		self.UpdateLEDs(self.BTN.LINK_LOCK, 0)
+		self.UpdateLEDs(self.BTN.PAN_FLIP, 0)
+		self.UpdateLEDs(self.BTN.CHANNEL_LOCK, 0)
+		self.UpdateLEDs(self.BTN.SCROLL_ZOOM, 0)
+
+		if mode == self.ENC.LINK:
+			self.UpdateLEDs(self.BTN.LINK_LOCK, PULSE_VAL)
+		elif mode == self.ENC.PAN:
+			self.UpdateLEDs(self.BTN.PAN_FLIP, PULSE_VAL)
+		elif mode == self.ENC.CHANNEL:
+			self.UpdateLEDs(self.BTN.CHANNEL_LOCK, PULSE_VAL)
+		elif mode == self.ENC.SCROLL:
+			self.UpdateLEDs(self.BTN.SCROLL_ZOOM, PULSE_VAL)
+	
+	def Next(self):
+		if PREV_NEXT_MODE == self.PN.MIXER:
+			mixer.setTrackNumber(mixer.trackNumber() + 1)
+		elif PREV_NEXT_MODE == self.PN.CHANNEL:
+			channels.selectOneChannel(channels.channelNumber() + 1)
+		else:
+			transport.globalTransport(midi.FPT_Next,1)
+	
+	def Previous(self):
+		if PREV_NEXT_MODE == self.PN.MIXER:
+			mixer.setTrackNumber(mixer.trackNumber() - 1) 
+		elif PREV_NEXT_MODE == self.PN.CHANNEL:
+			channels.selectOneChannel(channels.channelNumber() - 1)
+		else:
+			transport.globalTransport(midi.FPT_Previous,1)
+			
 
 	class BTN:
 		SOLO_CLEAR = 	8
@@ -230,8 +292,16 @@ class TFaderportV2:
 		FADER = 	224
 		KNOB = 		176
 		BTN = 		144
+	
+	class ENC:
+		LINK = 		1
+		PAN = 		2
+		CHANNEL = 	3
+		SCROLL = 	4
 
-
+	class PN:
+		MIXER = 	0
+		CHANNEL = 	1
 
 FaderportV2 = TFaderportV2()
 
@@ -244,23 +314,8 @@ def OnDeInit():
 def OnMidiIn(event):
 	FaderportV2.OnMidiIn(event)
 
-def OnMidiMsg(event):
-	FaderportV2.OnMidiMsg(event)
-
-def OnControlChange(event):
-	FaderportV2.OnControlChange(event)
-
-# def OnNoteOn(event):
-# 	FaderportV2.OnNoteOn(event)
-
-# def OnNoteOff(event):
-# 	FaderportV2.OnNoteOff(event)
-
-#def OnIdle():
-#	FaderportV2.OnIdle()
-
-def OnMidiOutMsg(event):
-	FaderportV2.OnMidiOutMsg(event)
+def OnIdle():
+	FaderportV2.OnIdle()
 
 def OnRefresh(Flags):
 	FaderportV2.OnRefresh(Flags)
